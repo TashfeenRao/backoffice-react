62a8edb4e1bd0924b2b031cdbb539d98a44ade98
tashfeenricult <tashfeen@ricult.com>
2023-12-29 00:03:37 +0500

    added skeleton for initial structure

---
+++ src/TaskCalender.js
@@ -0,0 +1,231 @@
+import "./TaskStyle.css";
+
+const TaskCalender = () => {
+  return (
+    <div className='task'>
+      <div className='rectangle-parent'>
+        <div className='frame-child' />
+        <div className='frame-item' />
+        <div className='frame-parent'>
+          <div className='wrapper'>
+            <div className='div'>8:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>9:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>10:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>11:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>12:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>13:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>14:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>15:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>16:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>17:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>18:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>19:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>20:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>21:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>22:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>23:00</div>
+          </div>
+          <div className='wrapper'>
+            <div className='div'>24:00</div>
+          </div>
+        </div>
+        <div className='frame-group'>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 1</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 2</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 3</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 4</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 5</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 6</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 7</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 8</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 9</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 10</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 11</div>
+          </div>
+          <div className='court-1-wrapper'>
+            <div className='court-1'>Court 12</div>
+          </div>
+        </div>
+        <div className='line-parent'>
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+          <div className='line-div' />
+          <div className='frame-inner' />
+        </div>
+        <div className='line-group'>
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+          <div className='frame-child32' />
+        </div>
+        <img className='rectangle-icon' alt='' src='/rectangle-2377@2x.png' />
+        <img className='line-icon' alt='' src='/line-73@2x.png' />
+        <div className='frame-container'>
+          <div className='wrapper14'>
+            <div className='div17'>8:00 - 9:00</div>
+          </div>
+          <div className='finn-daud-parent'>
+            <div className='finn-daud'>Finn Daud</div>
+            <div className='wrapper15'>
+              <div className='div18'>3/4</div>
+            </div>
+          </div>
+        </div>
+        <div className='frame-parent1'>
+          <div className='wrapper14'>
+            <div className='div17'>9:00 - 10:00</div>
+          </div>
+          <div className='finn-daud-parent'>
+            <div className='finn-daud'>Finn Daud</div>
+            <div className='wrapper17'>
+              <div className='div20'>2/4</div>
+            </div>
+          </div>
+        </div>
+        <div className='icon-chevronleft-wrapper'>
+          <img
+            className='icon-chevronleft'
+            alt=''
+            src='/icon--chevronleft@2x.png'
+          />
+        </div>
+        <div className='div21'>10:45</div>
+      </div>
+      <img className='task-child' alt='' src='/frame-427319089@2x.png' />
+      <div className='frame-parent2'>
+        <div className='october-2023-wrapper'>
+          <div className='court-1'>October, 2023</div>
+        </div>
+        <div className='frame-parent3'>
+          <div className='icon-chevronleft-container'>
+            <img
+              className='icon-chevronleft1'
+              alt=''
+              src='/icon--chevronleft@2x.png'
+            />
+          </div>
+          <div className='mon-parent'>
+            <div className='mon'>Mon</div>
+            <div className='div22'>9</div>
+          </div>
+          <div className='tue-parent'>
+            <div className='tue'>Tue</div>
+            <div className='div23'>10</div>
+          </div>
+          <div className='mon-parent'>
+            <div className='mon'>Wed</div>
+            <div className='div22'>11</div>
+          </div>
+          <div className='mon-parent'>
+            <div className='mon'>Thu</div>
+            <div className='div22'>12</div>
+          </div>
+          <div className='mon-parent'>
+            <div className='mon'>fri</div>
+            <div className='div22'>13</div>
+          </div>
+          <div className='mon-parent'>
+            <div className='mon'>sat</div>
+            <div className='div22'>14</div>
+          </div>
+          <div className='icon-chevronleft-container'>
+            <img
+              className='icon-chevronleft'
+              alt=''
+              src='/icon--chevronleft@2x.png'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCalender;
